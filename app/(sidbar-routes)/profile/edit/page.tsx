a0d602b17df29dce90302c0cdfd414f1cf9942a2
ahmed-dabos <ahmedghazydabos@gmail.com>
2025-10-25 16:06:39 +0300

    ^

--- app/(sidbar-routes)/profile/edit/page.tsx
+++ app/(sidbar-routes)/profile/edit/page.tsx
@@ -5,18 +5,23 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useRouter } from "next/navigation"
 import { editUser } from "@/actions/formActions"
+import  { useState } from "react"
 export default function EditUser() {
+    const [open, setOpen] = useState(false)
     const {user } = useUserContext()
     const router = useRouter()
     const editUserWithId = editUser.bind(null, user?.id)
     return (
-        <form action={editUserWithId} className="p-5 flex flex-col gap-1.5 md:flex-row lg:flex-row">
+        <form action={editUserWithId} className="p-5 flex flex-col gap-1.5 ">
             <Label htmlFor="username">username:</Label>
             <Input id="username" type="text" name="username" defaultValue={user?.username}/>
             <Label htmlFor="email">email:</Label>
             <Input id="email" type="email" name="email" defaultValue={user?.email}/>
             <Label htmlFor="password">password</Label>
-            <Input id="password" type="password" name="password" defaultValue={user?.password}/>
+            <div className="flex">
+            <Input id="password" type={open? "text" : "password"} name="password" defaultValue={user?.password}/>
+            <span onClick={() => setOpen(prev => !prev)} className="underline text-sm cursor-pointer">show password</span>
+            </div>
             <Input id="password" type="password" name="userId" defaultValue={user?.userId} hidden/>
             <Button type="button" onClick={() => router.push("/profile")}>cancel</Button>
             <Button type="submit">save</Button>
